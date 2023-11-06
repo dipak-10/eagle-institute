a217b8fa253bd36ad1e5e4960c1c361ce6b47a0d
dipak-10 <dk.patel102001@gmail.com>
2023-11-05 20:40:12 -0800

    -- added "institute_image" model field in institute details API -- added translator in base.html template page

--- institute/models.py
+++ institute/models.py
@@ -8,6 +8,7 @@ class Institute(models.Model):
     description_line_2 = models.CharField(max_length=10000)
     description_line_3 = models.CharField(max_length=10000, blank=True, null=True)
     description_line_4 = models.CharField(max_length=10000, blank=True, null=True)
+    institute_image = models.ImageField(upload_to='static/institute_images')
 
     class Meta:
         verbose_name = 'Institute Facilities'
